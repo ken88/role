5b622ee2cc0e549c421b40229d7e5ac990aae689
ZENSUNPC2018\Administrator <yukai08621@163.com>
2018-06-01 10:50:42 +0800

    时钟

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,6 +14,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'timeZone'=>'Asia/Shanghai',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
